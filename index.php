3b30a1a64086d2b444473709fb9b230c90f749b1
Gardenis <120688522+Gardenis@users.noreply.github.com>
2022-12-15 19:45:12 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<html>  
+<head>
+<style>
+
+#principal{
+		  border: solid1px violet;
+		  margin:0 auto;
+		  padding: 8px;
+		  width: 80%;
+	  }
+	  fieldset{
+		  margin-bottom:13px;
+	  }
+	  legend{
+		  border:solid 1px violet;
+		  padding:5px;
+		  font-weight: bold;
+		  border-radius: 25px;
+		  background:pink;
+	  }
+	  #texto{
+  display: inline-block;
+  vertical-align: top;
+}
+	  
+	  </style>
+	 
+<body>
+<link rel="stylesheet" href= "css/bootstrap.min.css"/>
+<div id= "principal">
+<center><img src="img/gardenis.jpg" alt=""class="img-thumbnail"/><p style="color: violet;"><font  size="20" face="comic Sans ms,Ariel, verdana"> Gardenis Lucas Trinidad</p></font></h1></center>
+<center><h3>Ingeniera en Sistemas</h3>
+<p>C/Respaldo Paraiso, no.16, Bo. Progreso II, klm17, Santo  Domingo,R.D.</p>
+<p> Cel: (809)-869-2968 <strong>Email:</strong> <a href="mailto:#">Gardenis00998@gmail.com</a></p></center>
+<br>
+<hr>
+
+<h2><p style="color: blue;"><font size="10" face="comic Sans ms,Ariel, verdana"> Objetivo</font></h2>
+<p>Formar parte de un equipo de trabajo y consolidarme profesionalmente en una empresa donde los logros personales y el desempeño sean reconocidos, además de permitir oportunidades de desarrollo personal y profesional.</p>
+
+<fieldset>
+<legend>Datos Generales</legend>
+<ul>
+
+<li>Cedula                         <strong> :402-3003375-1.</strong>
+<li>Fecha de Nacimiento             <strong>:03 de abril,2001.</strong>
+<li>Estado Civil                    <strong>:Soltera</strong>
+<li>Lugar de Nacimiento             <strong>:Yamasa</strong>
+<li>Nacionalidad                    <strong>:Dominicana</strong> </li>
+</ul>
+</fieldset>
+
+<fieldset>
+<legend>Formacion Academica</legend>
+<ul>
+<li>Universitaria(Ingenieria en sistemas): <strong>Universidad tecnologica de Santiago (UTESA)</strong>
+<LI>Bachiller concluido: <strong>Instituto Politecnico Max Henrriquez Ureña</strong> </li>
+</ul>
+</fieldset>
+
+<fieldset>
+<legend>Cursos Tecnico</legend>
+
+<ul>
+<fieldset>
+<li>Tecnica Profesional en informatica:  <strong>(IPMHU)</strong>
+<li> Auxiliar en contabilidad : <strong>ifotep</strong>
+<li>vendedora Externa:<strong> Infotep</strong>
+<li>Servivio al cliente :<strong>  CTC</strong>
+<li>Cajera Bancaria y Comercial: <strong>Infotep</strong>
+</ul>
+</li>
+
+</fieldset>
+
+<fieldset>
+<legend>Experiencia Laboral</legend>
+
+<ul>
+<fieldset>
+<li>Minimarket Los Hermanos II : <strong>(849)-433-3778- Encargada </strong>
+<li>Centro Tecnologico comunitario (CTC): <strong>Asistente Administrativa</strong>
+</ul>
+</li>
+
+</fieldset>
+
+<fieldset>
+<legend>Pelicula o Libro Favorito</legend>
+
+<ul>
+<fieldset>
+<<img src="img/avatar.jpg" alt=""/> <img src="img/momia.jpg" alt=""/> <img src="img/after.jpg" alt=""/>
+
+</ul>
+
+
+</fieldset>
+      </body>
+	  </head>
+	  </html>
